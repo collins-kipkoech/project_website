285b7bdc1f7219da8dee3752a142a11fe51ad04f
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-11-30 16:51:08 +0300

    fixing errors

--- project_website/settings.py
+++ project_website/settings.py
@@ -69,16 +69,17 @@ DEBUG = True
 INSTALLED_APPS = [
     'projects',
     'users',
+    'crispy_forms',
+    'cloudinary',
+    'rest_framework',
+    'rest_framework.authtoken',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
-    'cloudinary',
-    'rest_framework',
-    'rest_framework.authtoken',
+    
 ]
 
 MIDDLEWARE = [
